994223c20580ef206736e752e807f7afcf0437c9
Emilio Gomez Nef <126122650+Egonef@users.noreply.github.com>
2024-09-14 01:20:22 +0200

    Evitado el uso de clases dinamicas en el boton(menos problemas)

--- frontendtriviasd/src/components/standardButton.jsx
+++ frontendtriviasd/src/components/standardButton.jsx
@@ -3,11 +3,36 @@
 //Imports
 import React from "react";
 
-export default function StandardButton({ text, onClick , width , height}) {
+export default function StandardButton({ text, onClick , size}) {
+
+
+    let baseClasses = "bg-[#FF0033] text-white text-3xl font-bold py-2 px-4 rounded-2xl mx-16 my-5";
+
+
+    // Añadir clases de tamaño basadas en el argumento size
+    let sizeClasses;
+    switch (size) {
+        case 'small':
+            sizeClasses = "w-24 h-8"; // Ajusta estos valores según tus necesidades
+            break;
+        case 'medium':
+            sizeClasses = "w-56 h-24"; // Ajusta estos valores según tus necesidades
+            break;
+        case 'large':
+            sizeClasses = "w-72 h-36"; // Ajusta estos valores según tus necesidades
+            break;
+        default:
+            sizeClasses = "w-36 h-12"; // Sin tamaño adicional si 'size' no está definido o no coincide
+    }
+
+
+    let buttonClasses = `${baseClasses} ${sizeClasses}`;
+
     return (
         <button
         onClick={onClick}
-        className={`bg-[#FF0033] text-white text-3xl font-bold py-2 px-4 rounded-2xl w-${width} h-${height} mx-16 my-5`}        >
+        className={buttonClasses}>
+
         {text}
         </button>
     );
