b40e37f0ceff3be42a9c6d5789c19945eca1773d
Emilio Gomez Nef <126122650+Egonef@users.noreply.github.com>
2024-09-13 13:35:19 +0200

    Update administrador.js

--- frontendtriviasd/src/routes/administrador.js
+++ frontendtriviasd/src/routes/administrador.js
@@ -1,12 +1,12 @@
 // Pagina de registro de equipos
-// Date: 08/09/2021
+// WIP: Cambiar el boton por componente de boton estandar
 
 // Imports
-
+import React from 'react';
 
 //Componentes
 import Header from '../components/header';
-
+import StandardButton from '../components/standardButton';
 
 
 function AdminForm() {
@@ -14,30 +14,30 @@ function AdminForm() {
         <div className="App h-screen bg-gray-100">
             <Header/>
             <div className=" flex items-center flex-col h-[92%] pt-44"  >
-                <form className=" flex flex-row shadow-xl bg-gray-300  w-1/4 h-96 rounded-md p-8">
+                <form className=" flex flex-col shadow-xl bg-gray-300  w-1/4 h-96 rounded-md p-8">
                     <div className="flex flex-col justify-center h-auto mr-5">
                         <div className='flex flex-col  '>
-                            <label for="Name" className="block text-gray-700 text-md font-bold mb-2 ">
+                            <label htmlFor="Name" className="block text-gray-700 text-md font-bold mb-2 ">
                                 Nombre del equipo
                             </label>
                             <input type="text" id="Name" className="border-2 border-black rounded-lg "/>
                         </div>
                         <div className='flex flex-col mb-2 mt-5'>
-                            <label for="leaderName" className="block text-gray-700 text-md font-bold mb-2 ">
+                            <label htmlFor="leaderName" className="block text-gray-700 text-md font-bold mb-2 ">
 
                                 Nombre del líder
                             </label>
                             <input type="text" id="leaderName"  className="border-2 border-black rounded-lg "/>
                         </div>
                         <div className='flex flex-col mb-2 mt-5'>
-                            <label for="leaderMail" className="block text-gray-700 text-md font-bold mb-2 ">
+                            <label htmlFor="leaderMail" className="block text-gray-700 text-md font-bold mb-2 ">
 
                                 Correo del líder
                             </label>
                             <input type="text" id="leaderEmail"  className="border-2 border-black rounded-lg "/>
                         </div>
 
-                        <button type='button' className=" flex items-center justify-center self-center bg-[#FF0033] text-white  rounded-lg p-2 my-5 w-32 h-12 cursor-pointer" >Registrar</button>
+                        <StandardButton text="Registrar" onClick={(console.log("hehe"))} />
                     </div>
 
                 </form>
