f4458641164921294319f7f180880bdcbc74a932
Emilio Gomez Nef <126122650+Egonef@users.noreply.github.com>
2024-09-13 12:11:24 +0200

    Header en componente y botones

--- frontendtriviasd/src/App.js
+++ frontendtriviasd/src/App.js
@@ -4,20 +4,20 @@
 // Imports
 import './App.css';
 
+//Componentes
+import Header from './components/header';
 
 
 
 function App() {
     return (
         <div className="App h-screen bg-gray-100">
-            <header className="flex justify-center h-[8%] bg-[#FF0033]">
-                <img className=" " src="pingu.png" alt="logo" ></img>
-            </header>
+            <Header/>
             <div className=" flex items-center flex-col h-[92%] pt-44"  >
                 <h1 className=" text-4xl " ><b>Elige tu tipo de usuario:</b></h1>
-                <div className=" mt-20">
-                    <button className="bg-[#FF0033] text-white font-bold py-2 px-4 rounded-full w-40 h-20 m-4" >Jugador</button>
-                    <button className="bg-[#FF0033] text-white font-bold py-2 px-4 rounded-full w-40 h-20 m-4" >Administrador</button>
+                <div className=" mt-28">
+                    <button className="bg-[#FF0033] text-white text-3xl font-bold py-2 px-4 rounded-2xl w-72 h-36 mr-16" >Presentador</button>
+                    <button className="bg-[#FF0033] text-white text-3xl font-bold py-2 px-4 rounded-2xl w-72 h-36 ml-16" >Administrador</button>
                 </div>
             </div>
         </div>
---
+++ frontendtriviasd/src/components/header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+
+export default function Header() {
+    return (
+        <header className="flex justify-center h-[8%] bg-[#FF0033]">
+            <img className=" " src="pingu.png" alt="logo" ></img>
+        </header>
+    )
+}
+
